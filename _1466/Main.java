b44507725928071147090264819c18b2a4f56087
miaoweilun <2689890200@qq.com>
2024-08-22 10:04:26 +0800

    _39

---
+++ _1466/Main.java
@@ -0,0 +1,32 @@
+package _1466;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        List<int[]>[] e = new List[n];
+        for (int i = 0; i < n; i++) {
+            e[i] = new ArrayList<int[]>();
+        }
+        for (int[] edge : connections) {
+            e[edge[0]].add(new int[]{edge[1], 1});
+            e[edge[1]].add(new int[]{edge[0], 0});
+        }
+        return dfs(0, -1, e);
+    }
+
+    public int dfs(int x, int parent, List<int[]>[] e) {
+        int res = 0;
+        for (int[] edge : e[x]) {
+            if (edge[0] == parent) {
+                continue;
+            }
+            res += edge[1] + dfs(edge[0], x, e);
+        }
+        return res;
+    }
+}
+
+public class Main {
+}
