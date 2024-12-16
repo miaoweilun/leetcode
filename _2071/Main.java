440d503f9a1571f3b208aae4937e327cc730b709
miaoweilun <2689890200@qq.com>
2024-12-16 15:39:09 +0800

    测试提交

---
+++ _2071/Main.java
@@ -0,0 +1,13 @@
+package _2071;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+
+    }
+}
